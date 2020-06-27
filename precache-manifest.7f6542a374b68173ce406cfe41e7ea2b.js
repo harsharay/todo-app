4861bbdfaa3db0b64e02ad702e105c9387731a06
unknown <harsharay.ray@gmail.com>
2020-06-27 20:20:12 +0530

    Updates

---
+++ precache-manifest.7f6542a374b68173ce406cfe41e7ea2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94e1154d6260a3f5efde9550b9da966a",
+    "url": "/%7Brepo-name%7D/index.html"
+  },
+  {
+    "revision": "cb5b4d6eafe01fa9a4bf",
+    "url": "/%7Brepo-name%7D/static/css/main.ad1798a3.chunk.css"
+  },
+  {
+    "revision": "b223aebe4dab1f993086",
+    "url": "/%7Brepo-name%7D/static/js/2.39a9a34b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/%7Brepo-name%7D/static/js/2.39a9a34b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb5b4d6eafe01fa9a4bf",
+    "url": "/%7Brepo-name%7D/static/js/main.b64fe067.chunk.js"
+  },
+  {
+    "revision": "9262095a9fcbda1cc7d4",
+    "url": "/%7Brepo-name%7D/static/js/runtime-main.446b798a.js"
+  }
+]);
